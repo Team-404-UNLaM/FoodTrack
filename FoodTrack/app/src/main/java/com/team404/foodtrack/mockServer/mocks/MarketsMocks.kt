b8a1ecfd7b738a0390eeaf8e8d5a5d1dd720acce
Brian Bayarri <38357877+brianbayarri@users.noreply.github.com>
2022-05-25 17:17:13 -0300

    Implementation of google maps location (#50)
    * First iteration of maps
    
    * Fix map experience
    
    * Cleaning of code
    
    * Fix icons
    
    * Remove floting button

--- FoodTrack/app/src/main/java/com/team404/foodtrack/mockServer/mocks/MarketsMocks.kt
+++ FoodTrack/app/src/main/java/com/team404/foodtrack/mockServer/mocks/MarketsMocks.kt
@@ -6,6 +6,7 @@ import com.team404.foodtrack.data.MarketAddress
 object MarketsMocks {
 
     fun getMarketsMocks() : List<Market> {
+
         val bigPons = Market.Builder()
             .id(111111L)
             .name("Big Pons")
@@ -69,6 +70,54 @@ object MarketsMocks {
             .type(listOf("Bar", "Restaurant"))
             .build()
 
-        return listOf(bigPons, miGusto, malcriadaCafe, lucille)
+        val hood = Market.Builder()
+            .id(555555L)
+            .name("Hood")
+            .address(MarketAddress(
+                "Florencio Varela",
+                "1754",
+                "San Justo",
+                -34.67124749076973,
+            -58.56305038635756
+            ))
+            .marketImg("https://lh4.googleusercontent.com/-rkvi-cLfJVw/AAAAAAAAAAI/AAAAAAAAAAA/z5hxB2EZhxY/s44-p-k-no-ns-nd/photo.jpg")
+            .stars(4.8)
+            .description("La mejor hamburguesa del oeste, ¿Te la vas a perder?")
+            .type(listOf("Bar"))
+            .build()
+
+        val clubCervecero = Market.Builder()
+            .id(666666L)
+            .name("Club Cervecero UNLAM")
+            .address(MarketAddress(
+                "Pres. Juan Domingo Perón",
+                "2602",
+                "San Justo",
+                -34.6713802064953,
+                -58.563396991033954
+            ))
+            .marketImg("https://lh4.googleusercontent.com/-rkvi-cLfJVw/AAAAAAAAAAI/AAAAAAAAAAA/z5hxB2EZhxY/s44-p-k-no-ns-nd/photo.jpg")
+            .stars(3.2)
+            .description("Pizzas, hamburguesas y papitas frente a tu universidad favorita")
+            .type(listOf("Bar"))
+            .build()
+
+        val barEnfrente = Market.Builder()
+            .id(777777L)
+            .name("Bar de enfrente")
+            .address(MarketAddress(
+                "Florencio Varela",
+                "1940",
+                "San Justo",
+                -34.670456691404546,
+                -58.56220037928297
+            ))
+            .marketImg("https://lh4.googleusercontent.com/-rkvi-cLfJVw/AAAAAAAAAAI/AAAAAAAAAAA/z5hxB2EZhxY/s44-p-k-no-ns-nd/photo.jpg")
+            .stars(3.2)
+            .description("La mejor comida, la mejor musica, el mejor plan")
+            .type(listOf("Bar"))
+            .build()
+
+        return listOf(bigPons, miGusto, malcriadaCafe, lucille, hood, clubCervecero, barEnfrente)
     }
 }
