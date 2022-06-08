b15f1eaa49da15f77133c60f745f6d2613fcc892
Lucas Zacarias <lucas21111999@gmail.com>
2022-06-08 14:24:16 -0300

    interaction with WhatsApp added

--- FoodTrack/app/src/main/java/com/team404/foodtrack/ui/order/SelectOrderPaymentMethodFragment.kt
+++ FoodTrack/app/src/main/java/com/team404/foodtrack/ui/order/SelectOrderPaymentMethodFragment.kt
@@ -1,32 +1,26 @@
 package com.team404.foodtrack.ui.order
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.google.gson.GsonBuilder
-import com.squareup.picasso.Picasso
 import com.team404.foodtrack.R
-import com.team404.foodtrack.data.Market
 import com.team404.foodtrack.data.Order
-import com.team404.foodtrack.data.Product
 import com.team404.foodtrack.databinding.FragmentSelectOrderPaymentMethodBinding
-import com.team404.foodtrack.databinding.SelectProductsDrawerLayoutBinding
-import com.team404.foodtrack.domain.adapters.ExpandableSelectableMenuItemAdapter
-import com.team404.foodtrack.domain.factories.SelectedProductViewModelFactory
 import com.team404.foodtrack.domain.repositories.MarketRepository
-import com.team404.foodtrack.domain.repositories.MenuRepository
 import com.team404.foodtrack.domain.repositories.ProductRepository
-import com.team404.poketeam.domain.adapters.SelectedProductAdapter
 import org.koin.android.ext.android.inject
 
 class SelectOrderPaymentMethodFragment : Fragment() {
 
     private var _binding: FragmentSelectOrderPaymentMethodBinding? = null
-
+    private val marketRepository: MarketRepository by inject()
+    private val productRepository: ProductRepository by inject()
     private lateinit var order: Order.Builder
 
     private val binding get() = _binding!!
@@ -39,10 +33,54 @@ class SelectOrderPaymentMethodFragment : Fragment() {
         val root: View = binding.root
 
         order = GsonBuilder().create().fromJson(arguments?.getString("order"), Order.Builder::class.java)
-
+        makeMessageToSendByWhatsApp(order)
         setUpListeners()
 
         return root
+    }
+
+    private fun makeMessageToSendByWhatsApp(order: Order.Builder?) {
+        if(order !=null){
+            val messageToSend =
+                "Hola ${getMarketName(order.marketId)}\nAcabo de realizar el pedido #${order.id} de:\n${
+                    getProducts(order.products)
+                }\nPor la cantidad de: $${order.totalPrice}\n\nEquipo de Food Track"
+            val marketNumber = getMarketNumber(order.marketId)
+            if (marketNumber != null) {
+                sendMessageByWhatsApp(messageToSend, marketNumber)
+            }
+        }
+    }
+
+    private fun sendMessageByWhatsApp(messageToSend: String, marketNumber: String) {
+        val whatsAppIntent = Intent(Intent.ACTION_SEND)
+        whatsAppIntent.type = "text/plain"
+        whatsAppIntent.putExtra(Intent.EXTRA_TEXT, messageToSend)
+        whatsAppIntent.putExtra("jid", "$marketNumber@s.whatsapp.net")
+        whatsAppIntent.setPackage("com.whatsapp")
+        startActivity(whatsAppIntent)
+    }
+
+    private fun getMarketName(marketId: Long?): String? {
+        return marketId?.let { marketRepository.searchById(it).name }
+    }
+
+    private fun getMarketNumber(marketId: Long?): String? {
+        return marketId?.let { marketRepository.searchById(it) }?.cellPhone?.replace("+", "")
+            ?.replace(" ", "")
+    }
+
+    private fun getProducts(products: MutableMap<Long, Int>?): String {
+        var productsMessage = ""
+        products?.forEach { (key,value) ->
+            val productToAddToProductList = "${replaceProductKeyForProductName(key)} $value"+"u."+"\n"
+            productsMessage = productsMessage.plus(productToAddToProductList)
+        }
+        return productsMessage
+    }
+
+    private fun replaceProductKeyForProductName(key: Long): String? {
+        return productRepository.searchById(key).name
     }
 
     private fun setUpListeners() {
