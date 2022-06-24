333bb94d1c11c429aeb3f5e1bf320e5c3aaef69a
Lucas Zacarias <lucas21111999@gmail.com>
2022-06-24 14:17:24 -0300

    jetpack compose implementation

---
+++ FoodTrack/app/src/main/java/com/team404/foodtrack/ui/market/favs/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.team404.foodtrack.ui.market.favs.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val OrangeBackground = Color(0xFFFFD685)
+val OrangeInputText = Color(0xFFE65B00)
+val OrangeInputBackground = Color(0xFFFFE9BB)
