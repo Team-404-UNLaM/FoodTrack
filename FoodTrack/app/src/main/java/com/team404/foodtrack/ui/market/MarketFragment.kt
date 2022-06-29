07ae4e13f1ccef01e0fc01f2d7c4110f7c1c651a
Aylen <ailu.hoz28@gmail.com>
2022-06-29 04:54:50 -0300

    add map to market detail

--- FoodTrack/app/src/main/java/com/team404/foodtrack/ui/market/MarketFragment.kt
+++ FoodTrack/app/src/main/java/com/team404/foodtrack/ui/market/MarketFragment.kt
@@ -1,12 +1,26 @@
 package com.team404.foodtrack.ui.market
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.gson.GsonBuilder
 import com.squareup.picasso.Picasso
 import com.team404.foodtrack.R
@@ -19,6 +33,7 @@ import com.team404.foodtrack.domain.repositories.MarketFavoritesRepository
 import com.team404.foodtrack.domain.repositories.MarketRepository
 import org.koin.android.ext.android.inject
 
+@SuppressLint("MissingPermission")
 class MarketFragment : Fragment() {
 
     private var _binding: FragmentMarketBinding? = null
@@ -26,14 +41,44 @@ class MarketFragment : Fragment() {
     private lateinit var factory: MarketViewModelFactory
     private lateinit var viewModel: MarketViewModel
     private val marketRepository : MarketRepository by inject()
+    private var marketId: Long = 0L
     private lateinit var room: FoodTrackDB
+    private lateinit var map: GoogleMap
+
+    companion object {
+        const val REQUEST_CODE_LOCATION = 2008
+    }
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(requireContext(), R.raw.map_style))
+
+        val market = marketRepository.searchById(marketId)
+        val currentLocation = LatLng(market.address!!.latitude, market.address!!.longitude)
+
+        googleMap.addMarker(MarkerOptions()
+            .position(currentLocation)
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+        )
+
+        map = googleMap
+        enableLocation()
+
+        if (map.isMyLocationEnabled) {
+            googleMap.isMyLocationEnabled = true
+        }
+
+        googleMap.moveCamera(
+            CameraUpdateFactory.newLatLngZoom(currentLocation, 14f)
+        )
+
+    }
 
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View {
         _binding = FragmentMarketBinding.inflate(inflater, container, false)
         room = FoodTrackDB.getDatabase(requireContext())
         val root: View = binding.root
         val marketFavoritesRepository = MarketFavoritesRepository(room.marketFavoritesDao())
-        val marketId = arguments?.getLong("marketId")
+        marketId = arguments?.getLong("marketId")!!
 
         factory = MarketViewModelFactory(marketRepository, marketFavoritesRepository)
         viewModel = ViewModelProvider(this, factory).get(MarketViewModel::class.java)
@@ -45,6 +90,12 @@ class MarketFragment : Fragment() {
         }
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.market_map_location) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
     }
 
     private fun setUpListeners(root: View, marketId: Long) {
@@ -112,5 +163,53 @@ class MarketFragment : Fragment() {
             viewModel.changeMarketFavorite()
         }
 
+    }
+
+    private fun isLocationPermissionGranted() : Boolean {
+        return ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun enableLocation() {
+        if(!::map.isInitialized) return
+        if(isLocationPermissionGranted()) {
+            map.isMyLocationEnabled = true
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Toast.makeText(requireContext(), "Ve a ajustes y acepta los permisos", Toast.LENGTH_LONG).show()
+        } else {
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode) {
+            REQUEST_CODE_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    map.isMyLocationEnabled = true
+                } else {
+                    Toast.makeText(requireContext(), "Para activar la localizacion ve a ajustes y acepta los permisos", Toast.LENGTH_LONG).show()
+                }
+            }
+            else -> {}
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(!::map.isInitialized) return
+        if(!isLocationPermissionGranted()) {
+            map.isMyLocationEnabled = false
+            Toast.makeText(requireContext(), "Para activar la localizacion ve a ajustes y acepta los permisos", Toast.LENGTH_LONG).show()
+        }
     }
 }
