274ffcac54f7b56d0c1151b535cae3418ef16d0f
Brian Bayarri <bri_ezeba@hotmail.com>
2022-06-29 00:36:30 -0300

    Add general images

--- FoodTrack/app/src/main/java/com/team404/foodtrack/mockServer/mocks/ConsumptionModeMocks.kt
+++ FoodTrack/app/src/main/java/com/team404/foodtrack/mockServer/mocks/ConsumptionModeMocks.kt
@@ -9,13 +9,13 @@ object ConsumptionModeMocks {
         val consumptionMode1 = ConsumptionMode.Builder()
             .id(111111L)
             .name("Consumo en el local")
-            .consumptionModeImg("https://bigpons.com.ar/productos/20210422181717.jpeg")
+            .consumptionModeImg("https://i.pinimg.com/564x/79/42/4b/79424bc66709013581ff88a1858b3d42.jpg")
             .build()
 
         val consumptionMode2 = ConsumptionMode.Builder()
             .id(222222L)
             .name("Retiro por el local")
-            .consumptionModeImg("https://bigpons.com.ar/productos/20210422181717.jpeg")
+            .consumptionModeImg("https://cdn-icons-png.flaticon.com/512/1942/1942908.png")
             .build()
 
         return listOf(consumptionMode1, consumptionMode2)
--- FoodTrack/app/src/main/java/com/team404/foodtrack/mockServer/mocks/PaymentMethodMocks.kt
+++ FoodTrack/app/src/main/java/com/team404/foodtrack/mockServer/mocks/PaymentMethodMocks.kt
@@ -10,27 +10,39 @@ object PaymentMethodMocks {
         val paymentMethod1 = PaymentMethod.Builder()
             .id(111111L)
             .name("Efectivo")
-            .paymentMethodImg("https://bigpons.com.ar/productos/20210422181717.jpeg")
+            .paymentMethodImg("https://i.etsystatic.com/20162739/r/il/531799/1951524577/il_fullxfull.1951524577_h99n.jpg")
             .build()
 
         val paymentMethod2 = PaymentMethod.Builder()
             .id(222222L)
             .name("Mercado Pago")
-            .paymentMethodImg("https://bigpons.com.ar/productos/20210422181717.jpeg")
+            .paymentMethodImg("https://www.mgscreativa.com/images/jamp/page/logo-mercadopago29.png")
             .build()
 
         val paymentMethod3 = PaymentMethod.Builder()
             .id(333333L)
             .name("Tarjeta de débito")
-            .paymentMethodImg("https://bigpons.com.ar/productos/20210422181717.jpeg")
+            .paymentMethodImg("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDNItOubopRjMLl6IdNOCjPYaiS9WYp92gdQ&usqp=CAU")
             .build()
 
         val paymentMethod4 = PaymentMethod.Builder()
             .id(444444L)
             .name("Tarjeta de crédito")
-            .paymentMethodImg("https://bigpons.com.ar/productos/20210422181717.jpeg")
+            .paymentMethodImg("https://logowik.com/content/uploads/images/credit-card2790.jpg")
             .build()
 
-        return listOf(paymentMethod1, paymentMethod2, paymentMethod3, paymentMethod4)
+        val paymentMethod5 = PaymentMethod.Builder()
+            .id(555555L)
+            .name("Bitcoin")
+            .paymentMethodImg("https://i.etsystatic.com/11603536/r/il/834030/1426442143/il_fullxfull.1426442143_jjt8.jpg")
+            .build()
+
+        val paymentMethod6 = PaymentMethod.Builder()
+            .id(666666L)
+            .name("Ethereum ")
+            .paymentMethodImg("https://st3.depositphotos.com/5466018/15994/v/600/depositphotos_159947638-stock-illustration-coin-crypto-currency-ethereum-icon.jpg")
+            .build()
+
+        return listOf(paymentMethod1, paymentMethod2, paymentMethod3, paymentMethod4, paymentMethod5, paymentMethod6)
     }
 }
